d7af1d7c5717363e7cc970ecb4a1082a40b1dd04
sam2459 <sam2459@sina.cn>
2023-04-02 01:13:02 +0800

    基本完成,按下about可以弹窗

--- app/src/main/java/com/example/group_project/ui/Chat/ChatFragment.kt
+++ app/src/main/java/com/example/group_project/ui/Chat/ChatFragment.kt
@@ -1,16 +1,23 @@
 package com.example.group_project.ui.Chat
 
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.group_project.Local_user
 import com.example.group_project.R
 import com.example.group_project.databinding.FragmentChatBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -24,7 +31,7 @@ class ChatFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    val users =ArrayList<friend>()
+    val users = ArrayList<friend>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,14 +40,14 @@ class ChatFragment : Fragment() {
     ): View {
         _binding = FragmentChatBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val mRecyclerView=binding.recyclerUserView
+        val mRecyclerView = binding.recyclerUserView
         // Set its Properties using LinearLayout
         mRecyclerView.setLayoutManager(LinearLayoutManager(this@ChatFragment.context))
         // Set RecyclerView Adapter
         Log.d("Yes", Local_user.name)
 
-
-        val db = FirebaseDatabase.getInstance().getReference().child(Local_user.name).child("Friends")
+        val db =
+            FirebaseDatabase.getInstance().getReference().child(Local_user.name).child("Friends")
 
         db.addChildEventListener(object : ChildEventListener {
             override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
@@ -52,7 +59,7 @@ class ChatFragment : Fragment() {
                     users.add(comment!!)
                     val myAdapter = UserAdapter(this@ChatFragment.context!!, users)
                     mRecyclerView.setAdapter(myAdapter)
-                    Log.d("fire", users.size.toString()+comment.name + comment.msg)
+                    Log.d("fire", users.size.toString() + comment.name + comment.msg)
                 } catch (e: Exception) {
                     return
                 }
@@ -64,15 +71,23 @@ class ChatFragment : Fragment() {
             ) {
                 try {
 
+                    val CHANNEL_ID = "new_notice"
+                    var notification =
+                        NotificationCompat.Builder(this@ChatFragment.context!!, CHANNEL_ID)
+                            .setContentText("hi").build()
+
                     val comment: friend? = dataSnapshot.getValue(friend::class.java) ?: return
-                    if (comment != null) for(i in users.indices) {
+                    if (comment != null) for (i in users.indices) {
                         if (users[i].name == comment.name) {
                             users[i].msg = comment.msg
                             users[i].num = comment.num
                         }
                         val myAdapter = UserAdapter(this@ChatFragment.context!!, users)
                         mRecyclerView.setAdapter(myAdapter)
-                        Log.d("fire", "onChildChanged "+comment.name + comment.msg+comment.num.toString())
+                        Log.d(
+                            "fire",
+                            "onChildChanged " + comment.name + comment.msg + comment.num.toString()
+                        )
                     }
                 } catch (e: Exception) {
                     return
@@ -104,26 +119,42 @@ class ChatFragment : Fragment() {
         setHasOptionsMenu(true);
         return root
     }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_options, menu)
         super.onCreateOptionsMenu(menu!!, inflater)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar menu items
         when (item.itemId) {
-            R.id.add_friend->{
+            R.id.add_friend -> {
                 val intent = Intent(this@ChatFragment.context, AddFriend::class.java)
                 startActivity(intent)
                 return true
             }
             R.id.menu_about -> {
-                Toast.makeText(this@ChatFragment.context, "About Button Clicked !",
-                    Toast.LENGTH_LONG).show()
+                //Toast.makeText(this@ChatFragment.context, "About Button Clicked !", Toast.LENGTH_LONG).show()
+                val CHANNEL_ID = "new_notice"
+                val manager = context!!.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+                val fullScreenIntent = Intent(this@ChatFragment.context, ImportantActivity::class.java)
+                val fullScreenPendingIntent = PendingIntent.getActivity(this@ChatFragment.context, 0,
+                    fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                var builder = NotificationCompat.Builder(this@ChatFragment.context!!, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_user)
+                    .setContentTitle("sam")
+                    .setContentText("Hi!")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setFullScreenIntent(fullScreenPendingIntent, true)
+                manager.notify(1,builder.build())
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
